a3ecaf4c85f3b3a0b3135597d55d95228311971c
Justin Morris <jmortiger@gmail.com>
2025-06-24 09:27:50 -0400

    [MergePrep] Renamed `afficheSprite` to legacy name of `AffSpr`

--- FitdLib/aitdBox.cpp
+++ FitdLib/aitdBox.cpp
@@ -1,14 +1,15 @@
 #include "common.h"
 
-// #region AffSpfI/afficheSprite
-/* /// @brief
+// #region AffSpfI/AffSpr
+#ifdef __MISC_AFFICHE_SPRITE_REFACTOR__
+/// @brief Unifies `AffSpr` & `AffSprI`.
 /// @param left
 /// @param top
 /// @param index
 /// @param gfxData
 /// @param forceUpdate
-/// @details `afficheSprite` & `AffSpfI` are virtually identical, so this can be used to toggle which version is used by changing `forceUpdate`.
-void afficheSpriteShell(int left, int top, int index, char* gfxData, bool forceUpdate = false)
+/// @details Untested. `AffSpr` & `AffSpfI` are virtually identical, so this can be used to toggle which version is used by changing `forceUpdate`.
+void afficheSprite(int left, int top, int index, char* gfxData, bool forceUpdate = false)
 {
 	if (g_gameId >= AITD3)
 		return;
@@ -26,21 +27,24 @@ void afficheSpriteShell(int left, int top, int index, char* gfxData, bool forceU
 	int offset = _SCREEN_INTERNAL_WIDTH - width;
 
 	for (uint i = 0, j; i < height; i++, outPtr += offset) {
-		for (j = 0; j < width; j++, inPtr++, outPtr++) {
-			char color = *inPtr;
+		for (char color = *inPtr, j = 0; j < width; j++, color = *inPtr++, outPtr++) {
 			if (forceUpdate || color) *(outPtr) = color;
 		}
 	}
-} */
+}
+#endif
 
-/// @brief private; Poster sprite; Plaster the given sprite from the given graphics data at the specified point on the screen.
+/// @brief [private] afficheSprite; Poster sprite; Plaster the given sprite from the given graphics data at the specified point on the screen.
 /// @param left 
 /// @param top 
 /// @param index 
 /// @param gfxData 
-/// @note Seemingly had a legacy name of [AffSpf](<https://docs.google.com/spreadsheets/d/1cYRTP37v7Y11O38okNyHPg1YrZx549GG6z2vhY7QRok/edit?gid=2024760462#gid=2024760462&range=D297>)
-void afficheSprite(int left, int top, int index, char* gfxData)
+/// @note [Legacy name](<https://docs.google.com/spreadsheets/d/1cYRTP37v7Y11O38okNyHPg1YrZx549GG6z2vhY7QRok/edit?gid=2024760462#gid=2024760462&range=D297>)
+void AffSpr(int left, int top, int index, char* gfxData)
 {
+#ifdef __MISC_AFFICHE_SPRITE_REFACTOR__
+	afficheSprite(left, top, index, gfxData, true);
+#else
 	if (g_gameId >= AITD3)
 		return;
 
@@ -63,10 +67,10 @@ void afficheSprite(int left, int top, int index, char* gfxData)
 
 		outPtr += offset;
 	}
+#endif
 }
-// void afficheSprite(int left, int top, int index, char* gfxData) { afficheSpriteShell(left, top, index, gfxData, true); }
 
-/// @brief Same as `afficheSprite`, but only updates colors when the resultant color != 0
+/// @brief Same as `AffSpr`, but only updates colors when the resultant color != 0
 /// @param left 
 /// @param top 
 /// @param index 
@@ -74,6 +78,9 @@ void afficheSprite(int left, int top, int index, char* gfxData)
 /// @remark [Legacy name](<https://docs.google.com/spreadsheets/d/1cYRTP37v7Y11O38okNyHPg1YrZx549GG6z2vhY7QRok/edit?gid=2024760462#gid=2024760462&range=D298>)
 void AffSpfI(int left, int top, int index, char* gfxData)
 {
+#ifdef __MISC_AFFICHE_SPRITE_REFACTOR__
+	afficheSprite(left, top, index, gfxData, false);
+#else
 	if (g_gameId >= AITD3)
 		return;
 
@@ -98,9 +105,9 @@ void AffSpfI(int left, int top, int index, char* gfxData)
 
 		outPtr += offset;
 	}
+#endif
 }
-// void AffSpfI(int left, int top, int index, char* gfxData) { afficheSpriteShell(left, top, index, gfxData, false); }
-// #endregion AffSpfI/afficheSprite
+// #endregion AffSpfI/AffSpr
 
 /// @brief Poster Big Frame; Plaster the frame graphics data in `PtrCadre` at the specified point on the screen with the specified size.
 /// @param x The x coordinate of the frame's center.
@@ -135,7 +142,7 @@ void AffBigCadre(int x, int y, int width, int height)
 	right = x + halfWidth;
 	bottom = y + halfHeight;
 
-	afficheSprite(currentLeftPosition, currentTopPosition, 0, PtrCadre); // draw top left corner
+	AffSpr(currentLeftPosition, currentTopPosition, 0, PtrCadre); // draw top left corner
 
 	while (1) // draw top bar
 	{
@@ -144,10 +151,10 @@ void AffBigCadre(int x, int y, int width, int height)
 		if (right - 20 <= currentLeftPosition)
 			break;
 
-		afficheSprite(currentLeftPosition, currentTopPosition, 4, PtrCadre);
+		AffSpr(currentLeftPosition, currentTopPosition, 4, PtrCadre);
 	}
 
-	afficheSprite(currentLeftPosition, currentTopPosition, 1, PtrCadre); // draw top right corner
+	AffSpr(currentLeftPosition, currentTopPosition, 1, PtrCadre); // draw top right corner
 
 	currentLeftPosition = left;
 
@@ -158,21 +165,21 @@ void AffBigCadre(int x, int y, int width, int height)
 		if (bottom - 20 <= currentTopPosition)
 			break;
 
-		afficheSprite(currentLeftPosition, currentTopPosition, 6, PtrCadre);
+		AffSpr(currentLeftPosition, currentTopPosition, 6, PtrCadre);
 	}
 
 	currentLeftPosition = right - 8;
 	currentTopPosition = top + 20;
 
 	while (bottom - 20 > currentTopPosition) {
-		afficheSprite(currentLeftPosition, currentTopPosition, 7, PtrCadre);
+		AffSpr(currentLeftPosition, currentTopPosition, 7, PtrCadre);
 
 		currentTopPosition += 20;
 	}
 
 	currentLeftPosition = left;
 
-	afficheSprite(currentLeftPosition, currentTopPosition, 2, PtrCadre); // draw bottom left corner
+	AffSpr(currentLeftPosition, currentTopPosition, 2, PtrCadre); // draw bottom left corner
 
 	while (1) // draw bottom bar
 	{
@@ -181,12 +188,12 @@ void AffBigCadre(int x, int y, int width, int height)
 		if (right - 20 <= currentLeftPosition)
 			break;
 
-		afficheSprite(currentLeftPosition, currentTopPosition + 12, 5, PtrCadre);
+		AffSpr(currentLeftPosition, currentTopPosition + 12, 5, PtrCadre);
 	}
 
-	afficheSprite(currentLeftPosition, currentTopPosition, 3, PtrCadre); // draw bottom right corner
+	AffSpr(currentLeftPosition, currentTopPosition, 3, PtrCadre); // draw bottom right corner
 
-	afficheSprite(x - 20, currentTopPosition + 12, 8, PtrCadre); // draw "in the dark"
+	AffSpr(x - 20, currentTopPosition + 12, 8, PtrCadre); // draw "in the dark"
 
 	WindowX1 = left + 8;
 	WindowY1 = top + 8;
